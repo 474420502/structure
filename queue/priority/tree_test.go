58473d28cf325ebfbb8550c10631d61094056efa
eson <474420502@qq.com>
2021-08-18 16:16:53 +0800

    1. 优先队列随机对比测试完成

--- queue/priority/tree_test.go
+++ queue/priority/tree_test.go
@@ -21,15 +21,12 @@ func TestCase1(t *testing.T) {
 	for i := 0; i < 20; i++ {
 		v := rand.Intn(10)
 		q.Put(v, i)
-		log.Println(q.debugStringWithValue())
+		// log.Println(q.debugStringWithValue())
 	}
 
-	log.Println(q.debugStringWithValue())
-	log.Println(q.Gets(2))
 	q.Remove(2)
 	q.check()
-	log.Println(q.debugStringWithValue())
-	log.Println(q.Gets(2))
+
 }
 
 type tKey struct {
@@ -224,6 +221,8 @@ func TestRemoveForce(t *testing.T) {
 
 			}
 
+			queue.check()
+
 			r1 := fmt.Sprintf("%v", queue.Values())
 			r2 := fmt.Sprintf("%v", selectValues)
 			if r1 != r2 {
@@ -295,6 +294,8 @@ func TestRemoveRangeForce(t *testing.T) {
 			selectKeys = append(selectKeys, v.Key)
 		}
 
+		queue.check()
+
 		r1 := fmt.Sprintf("%v", queue.Values())
 		r2 := fmt.Sprintf("%v", selectValues)
 		if r1 != r2 {
@@ -349,6 +350,8 @@ func TestRemoveRangeByIndexForce(t *testing.T) {
 			selectValues = append(selectValues, v.Value)
 			selectKeys = append(selectKeys, v.Key)
 		}
+
+		queue.check()
 
 		r1 := fmt.Sprintf("%v", queue.Values())
 		r2 := fmt.Sprintf("%v", selectValues)
@@ -427,4 +430,170 @@ func TestPutGetsRemoveIndexForce(t *testing.T) {
 		}
 	}
 
+}
+
+func TestForce(t *testing.T) {
+	seed := time.Now().UnixNano()
+	log.Println(t.Name(), seed)
+	rand.Seed(seed)
+
+	for n := 0; n < 2000; n++ {
+		queue := New(compare.Int)
+		var priority []*tKey
+		var offset = 0.00001
+
+		for i := 0; i < 40; i++ {
+			v := rand.Intn(100)
+			queue.Put(v, v)
+			priority = append(priority, &tKey{float64(v) + offset, i})
+			offset += 0.00001
+		}
+
+		sort.Slice(priority, func(i, j int) bool {
+			return priority[i].Key < priority[j].Key
+		})
+
+		for i := 0; i < 5; i++ {
+			idx := rand.Intn(len(priority))
+			v := queue.RemoveIndex(int64(idx))
+			if int(priority[idx].Key) != v {
+				panic("")
+			}
+
+			if idx == len(priority)-1 {
+				priority = priority[0:idx]
+			} else {
+				priority = append(priority[0:idx], priority[idx+1:]...)
+			}
+		}
+
+		var same map[int]int = make(map[int]int)
+		for i := 0; i < len(priority)-1; i++ {
+			v1 := priority[i]
+			v2 := priority[i+1]
+
+			key1 := int(v1.Key)
+			key2 := int(v2.Key)
+
+			if key1 == key2 {
+				if _, ok := same[key1]; !ok {
+					same[key1] = 1
+				}
+				same[key1]++
+			}
+		}
+
+		for k, count := range same {
+			r := queue.Gets(k)
+			if len(r) != count {
+				panic("")
+			}
+
+			for _, v := range r {
+				if k != v {
+					panic("")
+				}
+			}
+		}
+
+		var sidx = rand.Intn(int(queue.Size()))
+		var eidx = rand.Intn(int(queue.Size()))
+		if sidx > eidx {
+			sidx, eidx = eidx, sidx
+		}
+
+		src := queue.debugStringWithValue()
+		queue.RemoveRangeByIndex(int64(sidx), int64(eidx))
+		eidx++
+		if eidx < len(priority) {
+			priority = append(priority[0:sidx], priority[eidx:]...)
+		} else {
+			priority = priority[0:sidx]
+		}
+
+		// var selectValues []int
+		var selectKeys []int
+		for _, v := range priority {
+			// selectValues = append(selectValues, v.Value)
+			selectKeys = append(selectKeys, int(v.Key))
+		}
+
+		r1 := fmt.Sprintf("%v", queue.Values())
+		r2 := fmt.Sprintf("%v", selectKeys)
+		if r1 != r2 {
+			log.Println(src)
+			log.Println(queue.debugStringWithValue(), sidx, eidx)
+			log.Println(r1)
+			log.Println(r2)
+			log.Panicln()
+		}
+
+		for i := 0; i < 40; i++ {
+			v := rand.Intn(200)
+			queue.Put(v, v)
+			priority = append(priority, &tKey{float64(v) + offset, i})
+			offset += 0.00001
+		}
+
+		sort.Slice(priority, func(i, j int) bool {
+			return priority[i].Key < priority[j].Key
+		})
+
+		sidx = rand.Intn(int(queue.Size()))
+		eidx = rand.Intn(int(queue.Size()))
+		if sidx > eidx {
+			sidx, eidx = eidx, sidx
+		}
+
+		var start = priority[sidx]
+		var end = priority[eidx]
+		for sidx > 0 && int(priority[sidx-1].Key) == int(start.Key) {
+			sidx--
+		}
+		for eidx != len(priority) && int(priority[eidx].Key) == int(end.Key) {
+			eidx++
+		}
+
+		src = queue.debugStringWithValue()
+		queue.RemoveRange(int(start.Key), int(end.Key))
+		if eidx < len(priority) {
+			priority = append(priority[0:sidx], priority[eidx:]...)
+		} else {
+			priority = priority[0:sidx]
+		}
+
+		// var selectValues []int
+		selectKeys = []int{}
+		for _, v := range priority {
+			// selectValues = append(selectValues, v.Value)
+			selectKeys = append(selectKeys, int(v.Key))
+		}
+
+		r1 = fmt.Sprintf("%v", queue.Values())
+		r2 = fmt.Sprintf("%v", selectKeys)
+		if r1 != r2 {
+			log.Println(src)
+			log.Println(queue.debugStringWithValue(), start.Key, end.Key, sidx, eidx)
+			log.Println(r1)
+			log.Println(r2)
+			log.Panicln()
+		}
+
+		queue.check()
+
+		if queue.Size() != 0 && len(priority) != 0 {
+			sidx = rand.Intn(int(queue.Size()))
+			v2 := priority[sidx]
+			_, v3 := queue.Index(int64(sidx))
+
+			if int(v2.Key) != v3 {
+				log.Println(queue.Values())
+				log.Println(priority)
+				log.Panicln(v3, v2.Value)
+			}
+		}
+
+		queue.Clear()
+		queue.check()
+	}
 }
